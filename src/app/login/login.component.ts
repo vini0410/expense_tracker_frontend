b04f9c325c7bec8075f096ec88bee5d13a595f52
vini0410 <vini0420@gmail.com>
2025-02-24 10:26:05 -0300

    add Reactive Login Form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,51 @@
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule, MatInputModule, MatButtonModule],
+  imports: [
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  constructor() { }
+  constructor() {}
+
+  form = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
+  });
 
   login() {
-    
+    let data = this.form.value;
+    console.log(data);
   }
 
+  get emailInvalid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+
+  get passwordInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
 }
