85ad1f9acac6d1eb60b10f6c06619a9f33282cce
vini0410 <vini0420@gmail.com>
2025-02-24 11:01:24 -0300

    add Reactive form Register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,19 +1,75 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-register',
-  imports: [MatCardModule, MatInputModule, MatButtonModule],
+  imports: [
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  constructor() { }
+  constructor() {}
+
+  form = new FormGroup({
+    name: new FormControl(null, [Validators.required, Validators.minLength(2)]),
+    email: new FormControl(null, {
+      validators: [Validators.required, Validators.email], // asyncValidators: [emailIsUnique],
+    }),
+
+    password: new FormControl(null, [Validators.required]), // mustContainQuestionMark
+  });
 
   register() {
-    
+    let data = this.form.value;
+    console.log(data);
   }
 
+  get nameInvalid() {
+    return (
+      this.form.controls.name.touched &&
+      this.form.controls.name.dirty &&
+      this.form.controls.name.invalid
+    );
+  }
+
+  get emailInvalid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+
+  get passwordInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
+
+  get buttonDisabled() {
+    if (
+      this.form.invalid ||
+      this.nameInvalid ||
+      this.emailInvalid ||
+      this.passwordInvalid
+    ) {
+      return true;
+    }
+    return false;
+  }
 }
